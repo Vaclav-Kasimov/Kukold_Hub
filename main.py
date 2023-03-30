28d0e244c3beca3c11d7c5208f971c8bfd6d5810
Vaclav-Kasimov <kasimov.vyacheslav.aaa@gmail.com>
2023-03-31 00:22:34 +0300

    login/logout implemented

--- main.py
+++ main.py
@@ -10,24 +10,21 @@ api_id = 20387754
 api_hash = 'c47bb9cdd6c140651113f118ae27d79f'
 client = TelegramClient('session_name', api_id, api_hash)
 
-@client.on(events.NewMessage(chats='schpecc'))
+@client.on(events.NewMessage(chats='leomatchbot'))
 async def msg_handler(event):
     print(event.message.to_dict()['message'])
     await client.log_out()
     client.disconnect()
 
-# async def login():
+@client.on(events.NewMessage(chats='schpecc'))
+async def msg_handler(event):
+    client.disconnect()
 
-#     await client.sign_in(phone)  # send code
-#
-#     code = input('enter code: ')
-#     await client.sign_in(phone, code)
-# asyncio.run(login())
-_phone = '+447960619415'
-
-
+def _phone():
+    return input('Дай телефон мразота\n')
 def get_code():
-    return input('kod dai sujka: ')
+    return input('Дай код гандон\n')
+
 client.start(phone = _phone, code_callback=get_code)
 
 client.run_until_disconnected()
